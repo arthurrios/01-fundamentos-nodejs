db02da81516bb85b8f3440067a80ae724690539d
arthur <arthur.rios007@gmail.com>
2024-08-01 21:25:03 -0300

    Capturando query parameters

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
+import { extractQueryParams } from './utils/extract-query-params.js'
 
 // Query Parameters: URL Stateful
 // Route Parameters: Identificação de recurso
@@ -26,6 +27,9 @@ const server = http.createServer(async (req, res) => {
   
   if (route) {
     const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+    req.query = query ? extractQueryParams(query) : {}
 
     req.params = { ...routeParams.groups }
 
